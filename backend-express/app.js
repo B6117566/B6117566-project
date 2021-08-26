82202d38bd718da07e1c2c31e14d220d166a75fe
b6117566 <b6117566@g.sut.ac.th>
2021-08-27 02:50:25 +0700

    write backend for test #1

---
+++ backend-express/app.js
@@ -0,0 +1,17 @@
+require('dotenv').config({ path: '../.env' });
+
+const address = require('../test/Address.json');
+
+// Require the framework and instantiate it
+const express = require('express');
+const app = express();
+
+// Declare a route
+app.get('/', (req, res) => {
+  res.send(address);
+});
+
+// Run the server!
+app.listen(process.env.PORT, () => {
+  console.log(`App listening on port => ${process.env.PORT}`);
+});
---
+++ backend-fastify/app.js
@@ -0,0 +1,23 @@
+require('dotenv').config({ path: '../.env' });
+
+const address = require('../test/Address.json');
+
+// Require the framework and instantiate it
+const fastify = require('fastify')({ logger: true });
+
+// Declare a route
+fastify.get('/', (request, reply) => {
+  reply.send(address);
+});
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(process.env.PORT);
+  } catch (error) {
+    fastify.log.error(error);
+    process.exit(1);
+  }
+};
+
+start();
