ca5e2a38d83cee93c29d3b95b2f7d82df56a4728
b6117566 <b6117566@g.sut.ac.th>
2021-09-10 17:35:39 +0700

    add route part to api v1

--- backend-express/routes/v1.js
+++ backend-express/routes/v1.js
@@ -1,6 +1,17 @@
 const expressFunction = require("express");
 const router = expressFunction.Router();
 
-router.use("/usertypes", require("./api/usertype"));
+router.use("/userroles", require("../api/userrole"));
+router.use("/users", require("../api/user"));
+router.use("/stocks", require("../api/stock"));
+router.use("/sizes", require("../api/size"));
+router.use("/provinces", require("../api/province"));
+router.use("/products", require("../api/product"));
+router.use("/orders", require("../api/order"));
+router.use("/genders", require("../api/gender"));
+router.use("/class", require("../api/class"));
+router.use("/categorys", require("../api/category"));
+router.use("/carts", require("../api/cart"));
+router.use("/address", require("../api/address"));
 
 module.exports = router;
