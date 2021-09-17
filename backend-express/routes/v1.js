203ccd0496d76202c6b535a56129031144e9aad0
b6117566 <b6117566@g.sut.ac.th>
2021-09-17 18:34:37 +0700

    update route backend

--- backend-express/routes/v1.js
+++ backend-express/routes/v1.js
@@ -2,16 +2,16 @@ const expressFunction = require("express");
 const router = expressFunction.Router();
 
 router.use("/userrole", require("../api/userrole"));
-//router.use("/user", require("../api/user"));
-//router.use("/stock", require("../api/stock"));
+router.use("/user", require("../api/user"));
+router.use("/stock", require("../api/stock"));
 router.use("/size", require("../api/size"));
 router.use("/province", require("../api/province"));
 router.use("/product", require("../api/product"));
-//router.use("/order", require("../api/order"));
+router.use("/order", require("../api/order"));
 router.use("/gender", require("../api/gender"));
 router.use("/class", require("../api/class"));
 router.use("/category", require("../api/category"));
-//router.use("/cart", require("../api/cart"));
+router.use("/cart", require("../api/cart"));
 router.use("/address", require("../api/address"));
 
 module.exports = router;
